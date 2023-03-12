dae03e34203ade46d82edb5bd67a9489d739b47e
Taras-Novitskyi <novitskyitaras@gmail.com>
2023-03-12 16:07:44 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/react-front-node">
+        <BrowserRouter basename="/react-front-node/">
           <App />
         </BrowserRouter>
       </PersistGate>
